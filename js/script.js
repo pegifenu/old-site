92fe870c520d4fa50a749021d68b0e6ed058b309
pegifenu <110061199+pegifenu@users.noreply.github.com>
2024-08-22 14:37:14 +0800

    sidebar navigation

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$("a").on("click", function(event) {
+    event.preventDefault();
+    let link = $(this).attr("href");
+    $("#home").load(link);
+});
+
+let arrow = document.querySelectorAll(".icon-link");
+for (var i = 0; i < arrow.length; i++) {
+    arrow[i].addEventListener("click", (e)=>{
+        let arrowParent = e.target.closest("li");
+        arrowParent.classList.toggle("showMenu");
+    });
+}
+
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
+sidebarBtn.addEventListener("click", ()=>{
+    sidebar.classList.toggle("close");
+});
