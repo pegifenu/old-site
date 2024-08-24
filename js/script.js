e0660626ee664b1371941bba682b22e0ef0986fc
pegifenu <110061199+pegifenu@users.noreply.github.com>
2024-08-25 04:12:58 +0800

    added proj filter

--- js/script.js
+++ js/script.js
@@ -18,3 +18,23 @@ let sidebarBtn = document.querySelector(".bx-menu");
 sidebarBtn.addEventListener("click", ()=>{
     sidebar.classList.toggle("close");
 });
+
+function searchProject() {
+    const input = document.querySelector(".box").value.toUpperCase();
+    const cards = $("#card-list .col");  // Using jQuery selector
+
+    cards.each(function() {
+        const card = $(this);
+        const title = card.find(".card-title").text().toUpperCase();
+
+        if (title.indexOf(input) > -1) {
+            // If card matches, show it (if hidden)
+            //card.show(200);
+            card.fadeIn(200);
+        } else {
+            // If card doesn't match, hide it with fadeOut
+            //card.hide(200);
+            card.fadeOut(200);
+        }
+    });
+}
