529a31b936f0c2fdf50116e483c3ba17e08b90a1
pegifenu <110061199+pegifenu@users.noreply.github.com>
2024-08-26 00:37:35 +0800

    lkjkl

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@ $("a").on("click", function(event) {
     event.preventDefault();
     let link = $(this).attr("href");
     $("#main").load(link);
+
+    if ($(this).parents().hasClass('sub-menu')) {
+        let category = $(this).attr("data-category");
+        filterProject(category)
+    }
 });
 
 let arrow = document.querySelectorAll(".icon-link");
@@ -44,15 +49,13 @@ function searchProject() {
 
 function filterProject(value) {
     const cards = document.getElementsByClassName("col");
-        
     for (let i = 0; i < cards.length; i++) {
         // Get the tags for this card
         const categories = cards[i].querySelector(".card").getAttribute("data-categories").split(' ');
         
         // Show or hide the card based on whether its tags include the selected tag
-        if (categories.includes(value)) {
-            console.log(categories);
-            cards[i].style.display = ""; // Show card
+        if (categories.includes(value)) {         
+            cards[i].style.display = "block"; // Show card
         } else {
             cards[i].style.display = "none"; // Hide card
         }
